ce01591bf94dbe8dd37080d1525a0ff27573ea09
AnuujinChadraa <anuujinchadraa@gmail.com>
2026-01-27 21:50:44 -0800

    made a card component reusable on home, resume boost, etc.

--- src/components/ActionCard/ActionCard.tsx
+++ src/components/ActionCard/ActionCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./ActionCard.css";
 
-type ActionCardProps = {
+interface ActionCardProps {
     icon: React.ReactNode;
     title: string;
     description: string;
